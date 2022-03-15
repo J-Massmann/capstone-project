d280ea4e42c620dc6dbcdb80dcf35b468e09046d
Justus Massmann <justusmassmann@online.de>
2022-03-15 14:41:16 +0100

    Add tests for form-component

---
+++ src/components/Form.spec.js
@@ -0,0 +1,22 @@
+import Form from './Form.js';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+
+describe('Form', () => {
+  it('renders two inputs, a selct and two button', () => {
+    render(
+      <MemoryRouter>
+        <Form />
+      </MemoryRouter>
+    );
+    const input1 = screen.getByLabelText(/destination/i);
+    const input2 = screen.getByLabelText(/location/i);
+    const select = screen.getByLabelText(/status/i);
+    const buttons = screen.getAllByRole('button');
+
+    expect(input1).toBeInTheDocument();
+    expect(input2).toBeInTheDocument();
+    expect(select).toBeInTheDocument();
+    expect(buttons).toHaveLength(2);
+  });
+});
