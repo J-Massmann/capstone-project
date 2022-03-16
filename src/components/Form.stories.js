fbf3c69a3ca4699326d1b7b4eb3e5f002dc34e83
Justus Massmann <justusmassmann@online.de>
2022-03-16 18:32:58 +0100

    Add Storybook

--- src/components/Form.stories.js
+++ src/components/Form.stories.js
@@ -1,9 +1,21 @@
 import FormAddTrip from './FormAddTrip.js';
+import FormEditTrip from './FormEditTrip.js';
 
 export default {
   title: 'Components/Form',
   component: FormAddTrip,
 };
 
-const Template = () => <FormAddTrip />;
-export const Default = Template.bind({});
+const TemplateAdd = () => <FormAddTrip />;
+export const Default = TemplateAdd.bind({});
+
+const templateEdit = args => <FormEditTrip {...args} />;
+export const Edit = templateEdit.bind({});
+Edit.args = {
+  destination: {
+    id: 123,
+    place: 'Barcelona',
+    isTripFuture: false,
+    locations: ['Park Güell'],
+  },
+};
