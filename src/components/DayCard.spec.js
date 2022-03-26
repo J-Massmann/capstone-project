0081b1120b177c5eb60834b26b2093a259a1a847
Justus Massmann <justusmassmann@online.de>
2022-03-26 13:06:58 +0100

    Correct test

--- src/components/DayCard.spec.js
+++ src/components/DayCard.spec.js
@@ -2,7 +2,7 @@ import DayCard from './DayCard.js';
 import { render, screen } from '@testing-library/react';
 
 describe('DayCard', () => {
-  it('renders an article with a header and a paragrpah', () => {
+  it('renders an article including the day and the date', () => {
     render(<DayCard dayNumber={1} routeNumber={1} date={'26.03.2022'} />);
 
     const day = screen.getByText('Day 1');
